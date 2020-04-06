6ba5f340b1ff056633ee4c2510ccdbd469349598
szpanel <szpanelek@gmail.com>
2020-04-06 17:47:24 +0200

    :wrench: Config for messages

---
+++ src/main/java/pl/szpanel/discordbot/data/configs/MessagesConfig.java
@@ -0,0 +1,8 @@
+package pl.szpanel.discordbot.data.configs;
+
+public class MessagesConfig {
+    public MessagesConfig() {
+    }
+
+    public String noRequiredRole = "Musisz posiadać rolę %role% aby wykonać tę akcję.";
+}
